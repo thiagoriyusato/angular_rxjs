7ca0a21cf0f3c48d69cbb00e8ad53fba0d8f7fe9
thiagosato <thiago.sato01@gmail.com>
2023-06-28 11:47:00 -0300

    Primeira Aula

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
-
   listaLivros: [];
+  campoBusca: string = '';
 
-  constructor() { }
+  constructor(private service: LivroService) {}
 
+  buscarLivros() {
+    this.service
+      .buscar(this.campoBusca)
+      .subscribe((retornoAPI) => console.log(retornoAPI));
+  }
 }
-
-
-
