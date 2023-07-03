a04d31f79d78a35387b09559514ebc249ccb0f62
thiagosato <thiago.sato01@gmail.com>
2023-07-03 09:05:09 -0300

    Segunda Aula

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes';
   constructor(private http: HttpClient) {}
 
-  buscar(valorDigitado: string): Observable<any> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get(this.API, { params });
   }
